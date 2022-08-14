db0d99d38dafc28d842fcc039248fa1a03690059
vladislav <ve@incsub.com>
2022-08-14 20:03:01 +0200

    Refactor file writer

---
+++ src/writer/file.rs
@@ -0,0 +1,53 @@
+use std::fs::File;
+use std::io::{BufWriter, Result, Write};
+
+use super::Writer;
+use crate::{Graph, ImageRenderer};
+
+pub struct FileWriter {
+    fname: String,
+}
+impl Writer for FileWriter {
+    fn write(&self, renderer: impl ImageRenderer, graph: &Graph) -> Result<()> {
+        let mut p = BufWriter::new(File::create(&self.fname)?);
+
+        let header = &renderer.get_header();
+        let footer = &renderer.get_footer();
+
+        if let Some(header) = header {
+            let _ = p.write(header)?;
+        }
+        let buffer = graph.draw(renderer);
+        let _ = p.write(&buffer)?;
+        if let Some(footer) = footer {
+            let _ = p.write(footer)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{ppm::Renderer, Block, Graph};
+
+    #[test]
+    fn graph_draw_save() {
+        let graph = Graph::new(&[
+            Block(4.0, 1.0),
+            Block(4.0, 3.0),
+            Block(4.0, 1.0),
+            Block(4.0, 2.0),
+        ]);
+        let w = FileWriter {
+            fname: String::from("foo.ppm"),
+        };
+        let renderer = Renderer::new(&graph.size);
+        if let Err(e) = w.write(renderer, &graph) {
+            assert!(false, "{:#?}", e);
+        } else {
+            assert!(true, "File created");
+        }
+    }
+}
--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -1,61 +1,17 @@
-use std::fs::File;
-use std::io::{BufWriter, Write};
+mod file;
+pub use file::*;
 
-use crate::{Graph,ImageRenderer};
+use std::io::Result;
+
+use crate::{Graph, ImageRenderer};
+
 trait Writer {
-    fn write(&self, renderer: impl ImageRenderer, graph: &Graph) -> std::io::Result<()>;
-}
-
-
-pub struct FileWriter {
-    fname: String,
-}
-impl Writer for FileWriter {
-    fn write(&self, renderer: impl ImageRenderer, graph: &Graph) -> std::io::Result<()> {
-        let mut p = BufWriter::new(File::create(&self.fname)?);
-
-        let header = &renderer.get_header();
-        let footer = &renderer.get_footer();
-
-        if let Some(header) = header {
-            let _ =p.write(&header)?;
-        }
-        let buffer = graph.draw(renderer);
-        let _ = p.write(&buffer)?;
-        if let Some(footer) = footer {
-            let _ = p.write(&footer)?;
-        }
-
-        Ok(())
-    }
+    fn write(&self, renderer: impl ImageRenderer, graph: &Graph) -> Result<()>;
 }
 
 pub struct StdoutWriter;
 impl Writer for StdoutWriter {
-    fn write(&self, _renderer: impl ImageRenderer, _graph: &Graph) -> std::io::Result<()> {
+    fn write(&self, _renderer: impl ImageRenderer, _graph: &Graph) -> Result<()> {
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::{Block, Graph, ppm::Renderer};
-
-    #[test]
-    fn graph_draw_save() {
-        let graph = Graph::new(&[
-            Block(4.0, 1.0),
-            Block(4.0, 3.0),
-            Block(4.0, 1.0),
-            Block(4.0, 2.0),
-        ]);
-        let w = FileWriter{fname: String::from("foo.ppm")};
-        let renderer = Renderer::new(&graph.size);
-        if let Err(e) = w.write(renderer, &graph) {
-            assert!(false, "{:#?}", e);
-        } else {
-            assert!(true, "File created");
-        }
     }
 }
