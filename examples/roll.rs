cb57b4c3407c49d5f06efaf2a8ec12e00ae9caf6
vladislav <ve@incsub.com>
2022-08-21 09:22:53 +0200

    Add roll graph example

---
+++ examples/roll.rs
@@ -0,0 +1,18 @@
+use graph::ppm::Renderer;
+use graph::writer::{FileWriter, Writer};
+use graph::{Block, Graph, Roll};
+
+fn main() -> std::io::Result<()> {
+    let blocks = [
+        Block::new(4.0, 1.0),
+        Block::new(4.0, 3.0),
+        Block::new(4.0, 1.0),
+        Block::new(4.0, 2.0),
+    ];
+    let graph = Roll::new(&blocks);
+    let w = FileWriter::new("foo.ppm");
+    let renderer = Renderer::new(&graph.size());
+    w.write(renderer, graph)?;
+
+    Ok(())
+}
